b50c12fae749c78c57f8a2af88563c10cadac3cd
attapolsa <30949933+attapolsa@users.noreply.github.com>
2021-02-03 08:25:59 +0700

    Add files via upload

---
+++ ProtectGroup.php
@@ -0,0 +1,64 @@
+<?php
+ $LINEData = file_get_contents('php://input');
+ $jsonData = json_decode($LINEData,true);
+ $replyToken = $jsonData["events"][0]["replyToken"];
+ $text = $jsonData["events"][0]["message"]["text"];
+ 
+ $servername = "27.254.174.46";
+ $username = "copyshop";
+ $password = "X93xPq1kl0";
+ $dbname = "shopeedb";
+
+ $mysql = new mysql_connect($servername, $username, $password, $dbname);
+ mysql_set_charset($mysql, "utf8");
+ 
+ if ($mysql->connect_error){
+ $errorcode = $mysql->connect_error;
+ print("MySQL(Connection)> ".$errorcode);
+ }
+ 
+ function sendMessage($replyJson, $token){
+   $ch = curl_init($token["URL"]);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+       'Content-Type: application/json',
+       'Authorization: Bearer ' . $token["AccessToken"])
+       );
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+   $result = curl_exec($ch);
+   curl_close($ch);
+return $result;
+}
+ 
+ $getUser = $mysql->query("SELECT * FROM 'test' WHERE 'question'='$text'");
+ $getuserNum = $getUser->num_rows;
+ 
+ if ($getuserNum == "0"){
+     $message = '{
+     "type" : "text",
+     "text" : "ไม่มีข้อมูลที่ต้องการ"
+     }';
+     $replymessage = json_decode($message);
+ } else {
+  
+   while(
+     $row = $getUser->fetch_assoc()){
+     $question = $row['question'];
+     $result = $row['result'];
+   }
+   $replymessage["type"] = "text";
+   $replymessage["text"] = $question." ".$result;
+ }
+ 
+ $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+ $lineData['AccessToken'] = "URuY3xDOh/DVmaTud2Oysb7Um/C06FnRYIxlomOImxOtmz6hbd9qS61ntXVRXHCfq9pvqqIrINNpbwIgbo8Hd9oJzVFQw2yir+j5+2HEtb6VZQ6v8hP09AVQOmYICqBQOndfAbCcqY7lnjZxUu5gkAdB04t89/1O/w1cDnyilFU=";
+ $replyJson["replyToken"] = $replyToken;
+ $replyJson["messages"][0] = $replymessage;
+ 
+ $encodeJson = json_encode($replyJson);
+ 
+ $results = sendMessage($encodeJson,$lineData);
+ echo $results;
+ http_response_code(200);
